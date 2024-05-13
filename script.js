58a16572d93ac8491e6e05a8322200f5f570a7d8
R-URU <russelljamesmapacpac12@gmail.com>
2024-05-13 14:53:58 +0800

    fix footer

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
  document.addEventListener('DOMContentLoaded', function(){
-    // values
+    //! values 🎗️ 
     let p1 = document.getElementById('p1');
     let textArea = document.getElementById('textArea');
     let p2 = document.getElementById('p2');
@@ -12,7 +12,7 @@
         navigator.clipboard.readText()
         .then (text => {
             document.getElementById('textArea').value += text;
-            updateWordCount(textArea, p1);
+            updateLetterCount(textArea, p1);
         })
         .catch(err => {
             console.log(err);
@@ -31,24 +31,25 @@
     });
 
     //* Word counter input 💪🏻
-    function updateWordCount(textArea, p1) {
+    function updateLetterCount(textArea, p1) {
         let text = textArea.value.trim();
-        let words1 = text.split(/\s+/).filter(word => word !== '');
-        p1.textContent = 'Word' + (words1.length !== 1 ? 's: ' : ': ') + words1.length;
+        p1.textContent = 'Letter' + (text.length !== 1 ? 's: ' : ': ') + text.length;
     }
     
-    function wordCount(textArea, p1) {
+    function letterCount(textArea, p1) {
         textArea.addEventListener('input', () => {
-            updateWordCount(textArea, p1);
+            updateLetterCount(textArea, p1);
         });
     }
-    wordCount(textArea, p1);
+    
+    letterCount(textArea, p1);
+    
 
     //* Word counter output ✨
     function updateWordCount2(outputArea, p2) {
         let text2 = outputArea.value.trim();
-        let word2 = text2.split(/\s+/).filter(word => word !== '');
-        p2.textContent = 'Word' + (word2.length !== 1 ? 's: ' : ': ') + word2.length;
+        // let words1 = text.split(/\s+/).filter(word => word !== '');
+        p2.textContent = 'Letter' + (text2.length !== 1 ? 's: ' : ': ') + text2.length;
     }
     
     function wordCount2(outputArea, p2) {
@@ -69,6 +70,30 @@
     function insertWord(inputString) {
         return inputString.split('').join('‎');
     }
+
+    //* alert show for copy 📁
+    window.copyText = function() {
+        var textArea = document.getElementById("outputArea");
+        if (textArea.value.trim() !== '') { // Check if textarea is not empty
+            var tempInput = document.createElement("textarea");
+            tempInput.value = textArea.value;
+            document.body.appendChild(tempInput);
+            tempInput.select();
+            document.execCommand("copy");
+            document.body.removeChild(tempInput);
+    
+            var copySuccess = document.getElementById("copySuccess");
+            copySuccess.classList.add("show");
+            setTimeout(function() {
+                copySuccess.classList.remove("show");
+            }, 1000);
+        }
+    }
+    
+
+
+
+
 });
 
 //! ‎
